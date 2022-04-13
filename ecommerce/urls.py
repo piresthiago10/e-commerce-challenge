b82a10c288d19284061477f904ec25ac72657688
“Thiago <tsantos@datastone.com.br>
2022-04-13 13:00:06 -0300

    comission endpoint

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -13,22 +13,28 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from customers.api.viewsets import CustomersViewSet
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework.routers import DefaultRouter
-from customers.api.viewsets import CustomersViewSet
+from django.views.decorators.csrf import csrf_exempt
 from products.api.viewsets import ProductsViewSet
-from sales.api.viewsets import SalesViewSet, SalesItemsViewSet
+from rest_framework.routers import DefaultRouter
+from sales.api.viewsets import SalesItemsViewSet, SalesViewSet, SalesDetailViewSet
 from sellers.api.viewsets import SellersViewSet
+from sellers import views as sellers_view
 
 router = DefaultRouter()
 router.register(r'costumers', CustomersViewSet, basename="clientes")
 router.register(r'products', ProductsViewSet, basename="produtos")
 router.register(r'sales', SalesViewSet, basename="vendas")
+router.register(r'detail_sales', SalesDetailViewSet, basename="detalhe_vendas")
 router.register(r'sales_items', SalesItemsViewSet, basename="itens_vendas")
 router.register(r'sellers', SellersViewSet, basename="vendedores")
 
 urlpatterns = [
+    path('api/comission/seller/<int:id>/start_date/<str:start_date>/end_date/<str:end_date>/',
+         csrf_exempt(sellers_view.SellersComission.as_view())),
+
     path('api/', include(router.urls)),
     path('admin/', admin.site.urls),
 ]

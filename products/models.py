576360970ec13a30b893cbd3784912b633e7c992
“Thiago <tsantos@datastone.com.br>
2022-04-14 04:39:03 -0300

    docstrings

--- products/models.py
+++ products/models.py
@@ -1,5 +1,7 @@
 import uuid
+
 from django.db import models
+
 
 class Product(models.Model):
     CHOICES_TYPE_PRODUCT = (
@@ -9,17 +11,24 @@ class Product(models.Model):
 
     bar_code = models.UUIDField(default=uuid.uuid4, editable=False)
     description = models.CharField(max_length=140)
-    type_product = models.CharField(max_length=7, choices=CHOICES_TYPE_PRODUCT, default='product')
+    type_product = models.CharField(
+        max_length=7, choices=CHOICES_TYPE_PRODUCT, default='product')
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.IntegerField()
     commission_percentage = models.DecimalField(max_digits=3, decimal_places=1)
 
     def verify_product_quantity(self, quantity: int) -> bool:
+        """
+        Ensure if the asked quantidy is enought in stock
+        """
         return self.quantity > quantity
 
-    def decrease_quantity(self, quantity:int) -> None:
+    def decrease_quantity(self, quantity: int) -> None:
+        """
+        Decrease the asked quantidy from stock
+        """
         self.quantity -= quantity
-        
+
         return self.save()
 
     def __str__(self):
